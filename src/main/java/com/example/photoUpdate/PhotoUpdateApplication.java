771c039961ed0438655409daac577f5d11260bd8
adanhuang0302 <adan.huang@tmnewa.com.tw>
2020-04-30 15:57:22 +0800

    first commit

---
+++ src/main/java/com/example/photoUpdate/PhotoUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoUpdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photoUpdate/PhotoUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoUpdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
